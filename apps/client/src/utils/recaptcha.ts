0d31ee3eb9b9bb974a27322a86d9dd6a4684e516
muazfanfix <231319859+muazfanfix@users.noreply.github.com>
2026-02-03 14:47:28 -0500

    Update recaptcha.ts

--- apps/client/src/utils/recaptcha.ts
+++ apps/client/src/utils/recaptcha.ts
@@ -21,7 +21,8 @@ declare global {
   }
 }
 
-const RECAPTCHA_SITE_KEY = '6Ld1XF0sAAAAAAAl0op6cWo88bp53vlWI0VHgYzI';
+// const RECAPTCHA_SITE_KEY = '6Ld1XF0sAAAAAAAl0op6cWo88bp53vlWI0VHgYzI'; // prod keys
+const RECAPTCHA_SITE_KEY = '6LdRN1osAAAAAEZqVEOq0MXLVlg___42QaXACNWq';
 const RECAPTCHA_SCRIPT_ID = 'recaptcha-script';
 
 /**
